fba00540fff69cb9d57eaa35b8624c3712bf823a
Jose Vidal <jvidalsm@jvidalsm-HP.(none)>
2014-02-03 20:54:57 -0500

    adding fs-variscite to config.sh

--- config.sh
+++ config.sh
@@ -30,7 +30,7 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"git://github.com/mozilla-b2g/b2g-manifest"}
+GITREPO=${GITREPO:-"git://github.com/jvidalsm/fs-b2g-manifest"}
 BRANCH=${BRANCH:-master}
 
 while [ $# -ge 1 ]; do
@@ -75,79 +75,8 @@ echo GECKO_OBJDIR=$PWD/objdir-gecko >> .tmp-config
 echo DEVICE_NAME=$1 >> .tmp-config
 
 case "$1" in
-"galaxy-s2")
-	echo DEVICE=galaxys2 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"galaxy-nexus")
-	echo DEVICE=maguro >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"nexus-4")
-	echo DEVICE=mako >> .tmp-config &&
-	repo_sync nexus-4
-	;;
-
-"nexus-5")
-  echo DEVICE=hammerhead >> .tmp-config &&
-  repo_sync nexus-5
-  ;;
-
-"optimus-l5")
-	echo DEVICE=m4 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"nexus-s")
-	echo DEVICE=crespo >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"nexus-s-4g")
-	echo DEVICE=crespo4g >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"otoro"|"unagi"|"keon"|"inari"|"leo"|"hamachi"|"peak"|"helix"|"wasabi")
+"fs-variscite")
 	echo DEVICE=$1 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"fugu")
-	echo DEVICE=fugu >> .tmp-config &&
-	echo LUNCH=fugu-eng >> .tmp-config &&
-	echo TARGET_HVGA_ENABLE=true >> .tmp-config &&
-	echo GONK_VERSION=SP7710_13A_W13.39.7 >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"tara")
-	echo DEVICE=sp8810ea >> .tmp-config &&
-	echo LUNCH=sp8810eabase-eng >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"pandaboard")
-	echo DEVICE=panda >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"emulator"|"emulator-jb")
-	echo DEVICE=generic >> .tmp-config &&
-	echo LUNCH=full-eng >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"emulator-x86"|"emulator-x86-jb")
-	echo DEVICE=generic_x86 >> .tmp-config &&
-	echo LUNCH=full_x86-eng >> .tmp-config &&
-	repo_sync $1
-	;;
-
-"flo")
-	echo DEVICE=flo >> .tmp-config &&
 	repo_sync $1
 	;;
 
@@ -156,28 +85,7 @@ case "$1" in
 	echo "Flags are passed through to |./repo sync|."
 	echo
 	echo Valid devices to configure are:
-	echo - galaxy-s2
-	echo - galaxy-nexus
-	echo - nexus-4
-	echo - nexus-s
-	echo - nexus-s-4g
-	echo - flo "(Nexus 7 2013)"
-	echo - otoro
-	echo - unagi
-	echo - inari
-	echo - keon
-	echo - peak
-	echo - leo
-	echo - hamachi
-	echo - helix
-	echo - wasabi
-	echo - fugu
-	echo - tara
-	echo - pandaboard
-	echo - emulator
-	echo - emulator-jb
-	echo - emulator-x86
-	echo - emulator-x86-jb
+	echo - fs-variscite
 	exit -1
 	;;
 esac
